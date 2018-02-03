f3f3eef3651660a9ee8ef52d4985157f5f619b0f
Ian Hunter <ianhunter2244@gmail.com>
2018-02-03 00:16:33 -0800

    First Unity Project
    Upload Unity to Git

---
+++ Pixelnest Tutorial/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector2 speed = new Vector2(50, 50);
+
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+
+        // ...
+
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+                SoundEffectsHelper.Instance.MakePlayerShotSound();
+            }
+        }
+
+        // ...
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        rigidbodyComponent.velocity = movement;
+    }
+
+    private void OnDestroy()
+    {
+        transform.parent.gameObject.AddComponent<GameOverScript>();
+
+        var gameOver = FindObjectOfType<GameOverScript>();
+        gameOver.ShowButtons();
+    }
+}
